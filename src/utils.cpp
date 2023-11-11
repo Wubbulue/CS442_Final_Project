64018b7b4996c8ee37965a5f24066099e1edcd0f
Jack-42 <johnjack0987@gmail.com>
2023-11-11 15:29:58 -0700

    add print_matrix() method

--- src/utils.cpp
+++ src/utils.cpp
@@ -1,1 +1,24 @@
-// utils code here
+#include <stdio.h>
+
+#include "utils.hpp"
+
+
+void print_matrix(bool* A, int nrows, int ncols) {
+    for (int i=0; i < nrows; i++) {
+        for (int j=0; j < ncols; j++) {
+            if (A[i*ncols + j]) {
+                printf("1 ");
+            }
+            else {
+                printf("0 ");
+            }
+        }
+        printf("\n");
+    }    
+    printf("\n");
+}
+
+
+void to_file(std::string fname, bool* A, int nrows, int ncols) {
+    // TODO
+}
