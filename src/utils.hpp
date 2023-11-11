1a2390aab52c30e3db7b98ff71e75f9f608c1b2a
Jack-42 <johnjack0987@gmail.com>
2023-11-10 17:58:39 -0700

    use 1d dynamic arrays instead

--- src/utils.hpp
+++ src/utils.hpp
@@ -4,13 +4,13 @@
 #include <fstream>
 #include <string>
 // load contents from fname into A
-void from_file(string fname, bool** A, int nrows, int ncols);
+void from_file(string fname, bool* A, int nrows, int ncols);
 // store contents of A into the provided file
-void to_file(string fname, bool** A, int nrows, int ncols);
+void to_file(string fname, bool* A, int nrows, int ncols);
 // copy contents of A into B
-void copy_matrix(bool** A, bool** B, int nrows, int ncols);
+void copy_matrix(bool* A, bool* B, int nrows, int ncols);
 // convert boolean arr to byte
 unsigned char to_byte(bool b[8]);
 // for debugging purposes
-void print_matrix(bool** A, int nrows, int ncols);
+void print_matrix(bool* A, int nrows, int ncols);
 #endif
